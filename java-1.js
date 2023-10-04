dd2a518be45751f065e541313f18041e3f446f66
Murad Alizada <144028740+MuraddAlizada@users.noreply.github.com>
2023-10-05 00:08:37 +0400

    Add files via upload

---
+++ java-1.js
@@ -0,0 +1,116 @@
+var q1 = confirm ("Bashlamaga hazirsiniz??")
+
+if ( q1==true ) {
+    console.log("Ugurlar");
+
+    var q2 = prompt ("21+15=?");
+    if ( q2 =="36" ) {
+        console.log("10 bal Qazandiniz! Sizin baliniz :10");
+        alert("10 bal Qazandiniz! Sizin baliniz :10");
+          
+    var q3 = prompt ("49+178=?");  
+    if ( q3 == "227" ) {
+        console.log("10 bal Qazandiniz! Sizin baliniz :20");
+        alert("10 bal Qazandiniz! Sizin baliniz :20");
+
+    var q4 = prompt ("65-29=?");
+    if ( q4 == "36" ) {
+        console.log("10 bal Qazandiniz! Sizin baliniz :30");
+        alert("10 bal Qazandiniz! Sizin baliniz :30");
+
+    var q5 = prompt ("427-289=?");
+    if ( q5 == "138" ) {
+        console.log("10 bal Qazandiniz! Sizin baliniz :40");
+        alert("10 bal Qazandiniz! Sizin baliniz :40");
+    
+    var q6 = prompt ("8*7=?");
+    if ( q6 == "56") {
+        console.log("Tebrikler siz 50 bal topladiniz!!!");
+        alert("Tebrikler siz 50 bal topladiniz!!!");
+
+    var q7 = prompt ("14*9=?");
+    if (q7 == "126" ) {
+        console.log("10 bal Qazandiniz! Sizin baliniz :60");
+        alert("10 bal Qazandiniz! Sizin baliniz :60");
+
+    var q8 = prompt ("327*4=?");
+    if (q8 == "1308" ) {
+        console.log("10 bal Qazandiniz! Sizin baliniz :70");
+        alert("10 bal Qazandiniz! Sizin baliniz :70");
+
+    var q9 = prompt ("63/7=?");
+    if ( q9 == "9" ) {
+        console.log("10 bal Qazandiniz! Sizin baliniz :80");
+        alert("10 bal Qazandiniz! Sizin baliniz :80");
+
+    var q10 = prompt ("468/78");
+    if ( q10 == "468") {
+        console.log(" 10 bal Qazandiniz! Sizin baliniz :90");
+        alert(" 10 bal Qazandiniz! Sizin baliniz :90");
+
+    var q11 = prompt ("5*(6+8)/7=?");
+    if ( q11 == "10" ) {
+        console.log(" 10 bal Qazandiniz! Sizin baliniz :100");
+        alert("TEBRIKLER!!! Siz butun suallara duzgun cavab verib 100 bal topladiniz!!!");
+    } else {
+        console.log("Cavabiniz sehvdir!! Sizin topladiginiz bal :90");
+        alert("Cavabiniz sehvdir!! Sizin topladiginiz bal :90");
+    }
+
+
+    } else {
+        console.log("Cavabiniz sehvdir!! Sizin topladiginiz bal :80");
+        alert("Cavabiniz sehvdir!! Sizin topladiginiz bal :80");
+    }
+    
+
+    } else {
+        console.log("Cavabiniz sehvdir!! Sizin topladiginiz bal :70");
+        alert("Cavabiniz sehvdir!! Sizin topladiginiz bal :70");
+    }
+
+    } else {
+        console.log("Cavabiniz sehvdir!! Sizin topladiginiz bal :60");
+        alert("Cavabiniz sehvdir!! Sizin topladiginiz bal :60");
+    }
+
+    } else {
+        console.log("Cavabiniz sehvdir!! Sizin topladiginiz bal :50");
+        alert("Cavabiniz sehvdir!! Sizin topladiginiz bal :50");
+    }
+
+
+    } else {
+        console.log("Cavabiniz sehvdir!! Sizin topladiginiz bal :40");
+        alert("Cavabiniz sehvdir!! Sizin topladiginiz bal :40");
+    }
+
+
+    } else {
+        console.log("Cavabiniz sehvdir!! Sizin topladiginiz bal :30");
+        alert("Cavabiniz sehvdir!! Sizin topladiginiz bal :30");
+    }
+
+
+    } else {
+        console.log("Cavabiniz sehvdir!! Sizin topladiginiz bal :20");
+        alert("Cavabiniz sehvdir!! Sizin topladiginiz bal :20");
+    }
+
+
+    } else {
+        console.log("Cavabiniz sehvdir!! Sizin topladiginiz bal :10");
+        alert("Cavabiniz sehvdir!! Sizin topladiginiz bal :10");
+    }
+
+
+
+    } else {
+        console.log("Cavabiniz sehvdir!!! siz bal toplaya bilmediniz!!");
+        alert("Cavabiniz sehvdir!!! siz bal toplaya bilmediniz!!");
+    }
+
+} else {
+    console.log("Sagolun!");
+    alert("Sagolun!");
+}
